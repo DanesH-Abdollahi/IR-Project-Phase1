f4461be4fcf5f3224ec5a5bead61e60ae5e24efc
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-04-07 00:26:13 +0330

    Modifications

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-from __future__ import unicode_literals
-from hazm.utils import stopwords_list
 import json
 from hazm import Normalizer, WordTokenizer, Lemmatizer
-from time import time
+from hazm.utils import stopwords_list
 
 with open("../IR_data_news_12k.json", "r") as file:
     data = json.load(file)
@@ -27,26 +25,17 @@ stopwords.extend(
         "}",
         "«",
         "»",
-        "،",
-        "؛",
         "؟",
         "-",
         "/",
     ]
 )
 
-# start = time()
 
 for id in data:
     tmp_tokens = []
     tokens_dict[id] = normalizer.normalize(data[id]["content"])  # normalize text
     tmp_tokens = tokenizer.tokenize(tokens_dict[id])  # list of tokens
-
-    if id == "2":
-        for i in tmp_tokens:
-            print(i)
-
-        break
 
     tmp_tokens = [
         token for token in tmp_tokens if token not in stopwords
@@ -57,20 +46,8 @@ for id in data:
 
     tokens_dict[id] = tmp_tokens
 
-# end = time()
 
-# print(f"Time : {end - start}")
-
-# print(len(tokens_dict.keys()))
-# print(tokens_dict["1"])
-# print("_______________________________________________________")
-# for i in tokens_dict["2"]:
+# for i in tokens_dict["0"]:
 #     print(i)
 
-# print("_______________________________________________________")
-# for i in stopwords:
-#     print(i)
-# print(stopwords_list())
-
-# print("_______________________________________________________")
-# print(data["2"]["content"])
+print(data["0"]["content"])
