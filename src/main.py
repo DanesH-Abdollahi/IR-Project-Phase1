c85d1b107acd4a61ada0ada89a05157f08620856
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-04-17 19:37:48 +0330

    Construct Inverted-Index

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from hazm.utils import stopwords_list
 with open("../IR_data_news_12k.json", "r") as file:
     data = json.load(file)
 
-normalizer = Normalizer()
+normalizer = Normalizer(token_based=True)
 tokenizer = WordTokenizer()
 lemmetizer = Lemmatizer()
 tokens_dict = dict()
@@ -33,7 +33,8 @@ stopwords.extend(
 
 for id in data:
     tmp_tokens = []
-    tokens_dict[id] = normalizer.normalize(data[id]["content"])  # normalize text
+    tokens_dict[id] = normalizer.normalize(
+        data[id]["content"])  # normalize text
     tmp_tokens = tokenizer.tokenize(tokens_dict[id])  # list of tokens
 
     tmp_tokens = [
@@ -43,4 +44,32 @@ for id in data:
     for i in range(len(tmp_tokens)):
         tmp_tokens[i] = lemmetizer.lemmatize(tmp_tokens[i])  # lemmatize tokens
 
-    tokens_dict[id] = tmp_tokens  # update tokens_dict with lemmatized tokens list
+    # update tokens_dict with lemmatized tokens list
+    tokens_dict[id] = tmp_tokens
+
+
+# Construct inverted index
+inverted_index = dict()
+
+for id in tokens_dict:
+    for token in tokens_dict[id]:
+        if token not in inverted_index:
+            inverted_index[token] = dict()
+            # inverted_index[token]["total_num"] = len(tokens_dict[id])
+            inverted_index[token]["total_num"] = 1
+            inverted_index[token]["PL"] = dict()
+
+            if id not in inverted_index[token]["PL"]:
+                inverted_index[token]["PL"][id] = 1
+            else:
+                inverted_index[token]["PL"][id] += 1
+        else:
+
+            inverted_index[token]["total_num"] += 1
+            if id not in inverted_index[token]["PL"]:
+                inverted_index[token]["PL"][id] = 1
+            else:
+                inverted_index[token]["PL"][id] += 1
+
+# Sort inverted index by token
+new_dict = dict(sorted(inverted_index.items(), key=lambda item: item[0]))
