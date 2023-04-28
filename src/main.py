d2e93f90c135e6e92fa691228097025cdfd0fa35
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-04-29 01:24:04 +0330

    Print outputs

--- src/main.py
+++ src/main.py
@@ -94,12 +94,14 @@ sorted_inverted_index = dict(
 query = input("Enter your query : ")
 query = normalizer.normalize(query)
 query = tokenizer.tokenize(query)
-query = [token for token in query if token not in stopwords or token == "!" or token == '«' or token == '»']
+query = [token for token in query if token not in stopwords or token ==
+         "!" or token == '«' or token == '»']
 for i in range(len(query)):
     query[i] = lemmetizer.lemmatize(query[i])
 
 # remove tokens that are not in inverted index to avoid key error
-query = [token for token in query if token in sorted_inverted_index.keys() or token == "!" or token == '«' or token == '»'] 
+query = [token for token in query if token in sorted_inverted_index.keys(
+) or token == "!" or token == '«' or token == '»']
 
 and_tokens = []
 not_tokens = []
@@ -111,13 +113,13 @@ phrase_flag = 0
 skip_flag = 0
 for i in range(len(query)):
     if skip_flag:
-        skip_flag -= 1 
+        skip_flag -= 1
         continue
 
     if query[i] == '!':
         not_flag = 1
         continue
-    
+
     if query[i] == '«':
         phrase_flag = 1
         continue
@@ -125,14 +127,14 @@ for i in range(len(query)):
     if not_flag == 1:
         if phrase_flag == 1:
             skip_flag = query.index('»', i) - i
-            not_phrase.append(query[i: i + skip_flag ])
+            not_phrase.append(query[i: i + skip_flag])
             phrase_flag = 0
             not_flag = 0
             continue
         not_tokens.append(query[i])
         not_flag = 0
         continue
-    
+
     if phrase_flag == 1:
         skip_flag = query.index('»', i) - i
         and_phrase.append(query[i: i + skip_flag])
@@ -142,13 +144,12 @@ for i in range(len(query)):
     and_tokens.append(query[i])
 
 
-
 intersection = set()
 union = set()
 refrence = set(data.keys())
 
 if and_tokens:
-    for i in range( len(and_tokens)):
+    for i in range(len(and_tokens)):
         if i == 0:
             intersection = set(sorted_inverted_index[and_tokens[i]].keys())
             union = intersection
@@ -158,19 +159,22 @@ if and_tokens:
             set(sorted_inverted_index[and_tokens[i]].keys()))
         union = union.union(
             set(sorted_inverted_index[and_tokens[i]].keys()))
-        
+
 if not_tokens:
     for i in range(len(not_tokens)):
         if not intersection:
-            intersection = refrence - set(sorted_inverted_index[not_tokens[i]].keys())
+            intersection = refrence - \
+                set(sorted_inverted_index[not_tokens[i]].keys())
             if not union:
                 union = intersection
-                continue 
+                continue
             union = union.union(intersection)
             continue
 
-        intersection = intersection.intersection(refrence - set(sorted_inverted_index[not_tokens[i]].keys()))
-        union = union.union(refrence - set(sorted_inverted_index[not_tokens[i]].keys()))
+        intersection = intersection.intersection(
+            refrence - set(sorted_inverted_index[not_tokens[i]].keys()))
+        union = union.union(
+            refrence - set(sorted_inverted_index[not_tokens[i]].keys()))
 
 
 if and_phrase:
@@ -178,19 +182,21 @@ if and_phrase:
         phrase_intersection = set()
         for i in range(len(phrase)):
             if i == 0:
-                phrase_intersection = set(sorted_inverted_index[phrase[i]].keys())
+                phrase_intersection = set(
+                    sorted_inverted_index[phrase[i]].keys())
                 continue
-    
+
             phrase_intersection = phrase_intersection.intersection(
                 set(sorted_inverted_index[phrase[i]].keys()))
-        
+
         phrase_intersection = phrase_intersection - set(['total_frequency'])
 
         tmp_phrase_intersection = phrase_intersection.copy()
         for id in tmp_phrase_intersection:
             tmp = sorted_inverted_index[phrase[0]][id]["positions"]
             for j in range(1, len(phrase)):
-                tmp = [x for x in tmp if x + j in sorted_inverted_index[phrase[j]][id]["positions"]]
+                tmp = [x for x in tmp if x +
+                       j in sorted_inverted_index[phrase[j]][id]["positions"]]
 
             if not tmp:
                 phrase_intersection.remove(id)
@@ -211,19 +217,21 @@ if not_phrase:
         phrase_intersection = set()
         for i in range(len(phrase)):
             if i == 0:
-                phrase_intersection = set(sorted_inverted_index[phrase[i]].keys())
+                phrase_intersection = set(
+                    sorted_inverted_index[phrase[i]].keys())
                 continue
-    
+
             phrase_intersection = phrase_intersection.intersection(
                 set(sorted_inverted_index[phrase[i]].keys()))
-        
+
         phrase_intersection = phrase_intersection - set(['total_frequency'])
 
         tmp_phrase_intersection = phrase_intersection.copy()
         for id in tmp_phrase_intersection:
             tmp = sorted_inverted_index[phrase[0]][id]["positions"]
             for j in range(1, len(phrase)):
-                tmp = [x for x in tmp if x + j in sorted_inverted_index[phrase[j]][id]["positions"]]
+                tmp = [x for x in tmp if x +
+                       j in sorted_inverted_index[phrase[j]][id]["positions"]]
 
             if not tmp:
                 phrase_intersection.remove(id)
@@ -232,20 +240,31 @@ if not_phrase:
             intersection = refrence - phrase_intersection
             if not union:
                 union = intersection
-                continue 
+                continue
             union = union.union(intersection)
             continue
 
-        intersection = intersection.intersection(refrence - phrase_intersection)
+        intersection = intersection.intersection(
+            refrence - phrase_intersection)
         union = union.union(refrence - phrase_intersection)
 
-intersection = intersection - set(['total_frequency']) # remove total_frequency from union
-union = union - set(['total_frequency']) # remove total_frequency from union
-union = union - intersection # remove intersection from union
+# remove total_frequency from union
+intersection = intersection - set(['total_frequency'])
+union = union - set(['total_frequency'])  # remove total_frequency from union
+union = union - intersection  # remove intersection from union
 
-intersection = sorted(intersection , key = lambda x : int(x))
-union = sorted(union , key = lambda x : int(x))
+intersection = sorted(intersection, key=lambda x: int(x))
+union = sorted(union, key=lambda x: int(x))
 query_result = intersection + union
+
+print(intersection)
+print(len(intersection))
+print("ـ"*50)
+print(union)
+print(len(union))
+print("ـ"*50)
+print(query_result)
+print(len(query_result))
 
 sentence_tokenizer = SentenceTokenizer()
 
@@ -253,10 +272,8 @@ if len(query_result) > 5:
     query_result_for_show = query_result[0:5]
 
 
-
 sentences = dict()
 for id in query_result_for_show:
-    # sentences[id] = []
     content = data[id]['content']
     content = normalizer.normalize(content)
     sentences[id] = sentence_tokenizer.tokenize(content)
@@ -270,16 +287,15 @@ for id in query_result_for_show:
             if query_token in tokens:
                 flag = 1
                 break
-        
-        
+
         for phrase in and_phrase:
             phrase_flag = 0
             if phrase[0] in tokens:
-                phrase_flag +=1
+                phrase_flag += 1
                 place = tokens.index(phrase[0])
                 for i in range(1, len(phrase)):
                     if phrase[i] == tokens[place + i]:
-                        phrase_flag +=1
+                        phrase_flag += 1
 
             if phrase_flag == len(phrase):
                 flag = 1
@@ -287,10 +303,10 @@ for id in query_result_for_show:
 
         if flag == 0:
             sentences[id].remove(sent)
-        
+
     sentences[id] = "".join(sentences[id])
-        
-    
+
+
 for id in query_result_for_show:
     print(f"Document ID :{int(id):< 6}")
     print(f"Title : {data[id]['title']}")
